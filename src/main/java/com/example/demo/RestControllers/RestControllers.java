f3f51d1972d0e2f14ceef0baaf7bfdf94fc792e9
vymdev <inversion83101229@gmail.com>
2026-04-19 23:47:05 -0400

    RestControllers obtain and send users, and perform data validation

--- src/main/java/com/example/demo/RestControllers/RestControllers.java
+++ src/main/java/com/example/demo/RestControllers/RestControllers.java
@@ -1,5 +1,9 @@
 package com.example.demo.RestControllers;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,4 +22,21 @@ public class RestControllers {
         this.service = service;
     } 
     
+    @GetMapping("/users")
+    public ResponseEntity<?> getUsers() {
+        return ResponseEntity.ok(service.GetAllUsers());
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<?> postUser(@RequestBody UserDTO entity) {
+        String user = entity.getUser();
+        String password = entity.getPassword();
+        if (user != null && !user.isEmpty() && password != null && !password.isEmpty()) {
+            service.AddUser(user,password);
+            return ResponseEntity.ok("User saved");
+        } else {
+            return ResponseEntity.badRequest().body("The password or username is empty.");
+        }
+    }
+
 }
