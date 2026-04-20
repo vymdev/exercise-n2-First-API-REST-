78c8ed0079a7d46b9529c94b74ca40e78ecf4b93
vymdev <inversion83101229@gmail.com>
2026-04-19 23:24:51 -0400

    A simple service that only works at runtime.

---
+++ src/main/java/com/example/demo/UserService.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    
+    public static List<String> User;
+    public static List<String> Password;
+
+    public void AddUser(String user, String password) {
+        this.User.add(user);
+        this.Password.add(password);
+    }
+
+    public List<String> GetAllUsers() {
+
+        List<String> result = new ArrayList<>();
+
+        for (int i = 0; i < User.size(); i++) {
+            result.add(User.get(i));
+            result.add(Password.get(i));
+        }
+
+        return result;
+
+    }
+}
